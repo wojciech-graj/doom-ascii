35d33ee703bd19ec1acf5e8baefdcec8d06a3d88
Wojciech Graj <wojtek.graj.2004@gmail.com>
2024-04-25 18:36:19 +0200

    Fix keybinds.

--- src/doomgeneric_ascii.c
+++ src/doomgeneric_ascii.c
@@ -1,5 +1,5 @@
 //
-// Copyright(C) 2022 Wojciech Graj
+// Copyright(C) 2022-2024 Wojciech Graj
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
