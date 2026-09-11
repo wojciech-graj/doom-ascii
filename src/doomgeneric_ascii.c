ce9f7eeb14cf1099b2a03007f919086a3a8be1e2
Wojciech Graj <me@w-graj.net>
2026-09-11 22:03:12 +0200

    Use bool from doomtype

--- src/doomgeneric_ascii.c
+++ src/doomgeneric_ascii.c
@@ -17,13 +17,13 @@
 
 #include "doomgeneric.h"
 #include "doomkeys.h"
+#include "doomtype.h"
 #include "i_system.h"
 #include "m_argv.h"
 
 #include <ctype.h>
 #include <errno.h>
 #include <locale.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
